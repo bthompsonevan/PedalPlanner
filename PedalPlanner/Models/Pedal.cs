228ba3c77c92c96f320faf924111f98549d0d3c6
bthompsonevan <bthompson.evan@gmail.com>
2020-02-28 21:28:29 -0800

    added pedal model and links to _layout nav bar

---
+++ PedalPlanner/Models/Pedal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PedalPlanner.Models
+{
+    public class Pedal
+    {
+        public int PedalID { get; set; }
+        public string PedalName { get; set; }
+        public string PedalType { get; set; }
+
+    }
+}
