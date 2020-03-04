173e7dd17faf09666a377fa0207d3120d4f0eba7
bthompsonevan <bthompson.evan@gmail.com>
2020-03-03 19:41:00 -0800

    added rig model

---
+++ PedalPlanner/Models/Rig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PedalPlanner.Models
+{
+    public class Rig
+    {
+        public int RigSetupID { get; set; }
+        public string Instrument { get; set; }
+        public int BoardSize { get; set; }
+
+    }
+}
