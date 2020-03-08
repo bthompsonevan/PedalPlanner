006afb2cbf18d5ec0c6ad74e98e3b5cf7384e43a
bthompsonevan <bthompson.evan@gmail.com>
2020-03-07 17:11:32 -0800

    removed unused privacy and pedalcollection get action methods

--- PedalPlanner/Controllers/HomeController.cs
+++ PedalPlanner/Controllers/HomeController.cs
@@ -27,17 +27,7 @@ namespace PedalPlanner.Controllers
         {
             return View();
         }
-
-        public IActionResult PedalCollection()
-        {
-            return View();
-        }
-    
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+         
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
