c027664258829ace09572668d9dd9086ca10e32a
bthompsonevan <bthompson.evan@gmail.com>
2020-03-15 13:12:25 -0700

    added validation to pedal and rig models and drop down menu to pedal colors

--- PedalPlanner/Models/Pedal.cs
+++ PedalPlanner/Models/Pedal.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,15 @@ namespace PedalPlanner.Models
     public class Pedal
     {
         public int PedalID { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 1, ErrorMessage = "Pedal Name must be at least one charater, but less than 30")]
         public string PedalName { get; set; }
+        [Required]
         public string PedalType { get; set; }  //  Example: Distortion Pedal
+        [Required]
+        [StringLength(30, MinimumLength = 1, ErrorMessage = "Pedal Sub Type must be at least one charater, but less than 30")]
         public string PedalSubType { get; set; }  // Example: Fuzz
+        [Required]
         public string PedalColor { get; set; }  // Example:  Distortion is orange - base off of line6 color scheme
         public string CreatedBy { get; set; }
 
--- PedalPlanner/Models/Rig.cs
+++ PedalPlanner/Models/Rig.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +10,13 @@ namespace PedalPlanner.Models
     public class Rig
     {
         public int RigID { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "Must be at least 4 characters long.")]
         public string Instrument { get; set; }
+        [Required]
+        [Range(1, 30, ErrorMessage = "Pedalboard size must be greater than one and less than 30")]
         public int BoardSize { get; set; }
+        [Required]        
         public string CreatedBy { get; set; }
        
         
